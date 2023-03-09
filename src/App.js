85e89c00ed3387cb014a06a3b2213276357db6b5
Gayatri Puppala <gayatri.puppala03@gmail.com>
2023-03-09 15:59:40 -0800

    attempted implement python script to add to json but commented out

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
 import { useState } from "react";
 
+//  const addParty = () => {
+//      const python_code = `
+//         import json
+//                 entry = {
+//                   "id": 11,
+//                   "name": "Random",
+//                   "banner": "http://dummyimage.com/231x100.png/ff4444/ffffff",
+//                   "price": 11.11,
+//                   "date": "2022-08-29 19:14:13"
+//         }
+//         #read file content
+//         with open(JSONDATA, "r") as file:
+//         data = json.load(file)
+
+//         #update json Object
+//         data.append(entry)
+
+//         #write json file
+//         with open(JSONDATA, "w") as file:
+//         json.dump(data, file)
+//               print("hello")
+//             `;
+
+//     const pyodide = window.pyodide;
+
+//     pyodide.runPython(python_code);
+// };
+
 function App() {
     //need to be able to change searchTeerm
     const [searchTerm, setSearchTerm] = useState("");
